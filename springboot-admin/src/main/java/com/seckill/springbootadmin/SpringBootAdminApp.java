37676dee470dbbe515e5b7826b568877be26dd27
1109546392@qq.com <1109546392@qq.com>
2019-04-17 19:04:13 +0800

    回退

--- springboot-admin/src/main/java/com/seckill/springbootadmin/SpringBootAdminApp.java
+++ springboot-admin/src/main/java/com/seckill/springbootadmin/SpringBootAdminApp.java
@@ -3,10 +3,12 @@ package com.seckill.springbootadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+//@Configuration
+//@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class SpringBootAdminApp {
 
